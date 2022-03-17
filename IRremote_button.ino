8a56a58985ed00ce2ea4a04d1b5b0932b467f2ae
chungchachi <100845371+chungchachi@users.noreply.github.com>
2022-03-17 19:26:19 +0800

    Update IRremote_button.ino

--- IRremote_button.ino
+++ IRremote_button.ino
@@ -11,10 +11,19 @@ volatile byte state = LOW;
 #define DECODE_DISTANCE     // in case NEC is not received correctly
 
 #include "PinDefinitionsAndMore.h"
+//#define EXCLUDE_UNIVERSAL_PROTOCOLS // Saves up to 1000 bytes program space.
+//#define EXCLUDE_EXOTIC_PROTOCOLS
+//#define SEND_PWM_BY_TIMER
+//#define USE_NO_SEND_PWM
+//#define NO_LED_FEEDBACK_CODE // saves 500 bytes program space
+//#define DEBUG // Activate this for lots of lovely debug output from the decoders.
 #include <IRremote.hpp>
 #define INFO
 #define DELAY_AFTER_SEND 2000
 #define DELAY_AFTER_LOOP 5000
+uint16_t sAddress = 0x0102;
+uint8_t sCommand = 0x34;
+uint8_t sRepeats = 1;
  
 void setup() {
   Serial.begin(9600);
@@ -25,17 +34,33 @@ void setup() {
 
 void loop() {
   Serial.println("IR receiving...");
+if (IrReceiver.decode()) {
+        Serial.print(F("Decoded protocol: "));
+        Serial.print(getProtocolString(IrReceiver.decodedIRData.protocol));
+        Serial.print(F("Decoded raw data: "));
+        Serial.print(IrReceiver.decodedIRData.decodedRawData, HEX);
+        Serial.print(F(", decoded address: "));
+        Serial.print(IrReceiver.decodedIRData.address, HEX);
+        Serial.print(F(", decoded command: "));
+        Serial.println(IrReceiver.decodedIRData.command, HEX);
+        IrReceiver.resume();
+    }
   /* Receiver code of SendAndReceive.ino */
 }
 
 void IRsender() {
   Serial.println("IR sending...");
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(115200);
-  Serial.println(F("START " _FILE_ " from "_DATE_"\r\nUsing library version " VERSION_IRREMOTE));
-  IrSender.begin();
-  Serial.print(F("Ready to send IR signals at Pin"));
-  Serial.println(IR_SEND_PIN);
+  Serial.print(F("Sending: 0x"));
+    Serial.print(sAddress, HEX);
+    Serial.print(sCommand, HEX);
+    Serial.println(sRepeats, HEX);
+
+    // clip repeats at 4
+    if (sRepeats > 4) {
+        sRepeats = 4;
+    }
+    // Results for the first loop to: Protocol=NEC Address=0x102 Command=0x34 Raw-Data=0xCB340102 (32 bits)
+    IrSender.sendNEC(sAddress, sCommand, sRepeats);
   
   /* Sender of SendAndReceiv.ino */
 }
