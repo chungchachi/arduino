147eaddcf5ef321d7be45e91383542ce296ef0de
chungchachi <100845371+chungchachi@users.noreply.github.com>
2022-03-17 19:09:39 +0800

    Update IRremote_button.ino

--- IRremote_button.ino
+++ IRremote_button.ino
@@ -1,33 +1,41 @@
 #include <Arduino.h>
-#include"PinDefinitionsAndMore.h"
-#include <IRremote.hpp>
-#define DECODE_NEC
-#define LED_BUILTIN 2
 
-const byte ledPin = 2;       
-const byte interruptPin = 0; 
+/* Defines of GPIO_Interrupt.ino */
+const byte ledPin = 2;       // Builtin-LED pin
+const byte interruptPin = 0; // BOOT/IO0 button pin
 volatile byte state = LOW;
 
-void setup() 
-{
+/* Defines of SendAndReceive.ino */
+// select only NEC and the universal decoder for pulse width or pulse distance protocols
+#define DECODE_NEC          // Includes Apple and Onkyo
+#define DECODE_DISTANCE     // in case NEC is not received correctly
+
+#include "PinDefinitionsAndMore.h"
+#include <IRremote.hpp>
+#define INFO
+#define DELAY_AFTER_SEND 2000
+#define DELAY_AFTER_LOOP 5000
+ 
+void setup() {
+  Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
   pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
-  
+  attachInterrupt(digitalPinToInterrupt(interruptPin), IRsender, CHANGE);
+}
+
+void loop() {
+  Serial.println("IR receiving...");
+  /* Receiver code of SendAndReceive.ino */
+}
+
+void IRsender() {
+  Serial.println("IR sending...");
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
   Serial.println(F("START " _FILE_ " from "_DATE_"\r\nUsing library version " VERSION_IRREMOTE));
   IrSender.begin();
   Serial.print(F("Ready to send IR signals at Pin"));
   Serial.println(IR_SEND_PIN);
-}
-
-void loop() 
-{
-  digitalWrite(ledPin, state);
-}
-
-void blink() 
-{
-  state = !state;
+  
+  /* Sender of SendAndReceiv.ino */
 }
